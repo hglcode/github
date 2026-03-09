81a4a78c957da2507c35e7988146acb5b257f78b
sam <hgl9@foxmail.com>
2026-03-10 01:39:55 +0800

    initial commit.

--- gitdns.sh
+++ gitdns.sh
@@ -38,13 +38,17 @@ add_github_hosts() {
 }
 
 get_github_dns() {
+    uri_a=https://raw.githubusercontent.com/521xueweihan/GitHub520/refs/heads/main/hosts
+    uri_b=https://githubhosts.xuanyuan.me
     # 从主要源获取
-    dns=$(curl -s https://raw.githubusercontent.com/521xueweihan/GitHub520/refs/heads/main/hosts | grep -oP '(\d+\.){3}\d+\s+(\w+\.)+\w+')
+    dns=$(curl -s "$uri_a" | grep -oiE '(([0-9]+\.){3}[0-9]+|[0-9a-f:]{8,}:[0-9a-f]{1,4})\s+(\w+\.)+\w+')
 
    # 如果主要源失败，从备用源获取
     if [ -z "$dns" ]; then
-        dns=$(wget -q -O -  https://githubhosts.xuanyuan.me | grep -oP '#Github Hosts Start.*#Github Hosts End' | grep -oP '(\d+\.){3}\d+\s+(\w+\.)+\w+')
+        dns=$(wget -q -O - "$uri_b" | grep -oE '#Github Hosts Start.*?#Github Hosts End' | grep -ioE '(([0-9]+\.){3}[0-9]+|[0-9a-f:]{8,}:[0-9a-f]{1,4})\s+(\w+\.)+\w+')
     fi
+    # 提取包含IPv4和IPv6的Github Hosts记录
+dns=$(wget -q -O - "$uri_b" | grep -oE '#Github Hosts Start.*?#Github Hosts End' | grep -oE '(([0-9]+\.){3}[0-9]+|(([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))\s+(\w+\.)+\w+')
 
     echo "$dns"
 }
@@ -69,7 +73,8 @@ main() {
     fi
 
     echo "Getting latest GitHub DNS entries..."
-    dns_github=$(get_github_dns)
+    # shellcheck disable=SC2068
+    dns_github=$(get_github_dns $@)
 
     # 检查是否获取到DNS条目
     if [ -z "$dns_github" ]; then
@@ -92,4 +97,6 @@ main() {
 }
 
 # 执行主函数
-main
+
+# shellcheck disable=SC2068
+main $@
