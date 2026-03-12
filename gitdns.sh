3dc2271d82ef8af80cb347fa22a8952b328e8c31
sam <hgl9@foxmail.com>
2026-03-12 10:43:35 +0800

    feat: support do not rewrite if dns is updated.

--- gitdns.sh
+++ gitdns.sh
@@ -9,9 +9,9 @@ fi
 cleanup_github_hosts() {
     hosts=$1
     # 移除所有Github Hosts相关条目
-    sed -i '/^\s*#\s*Github Hosts Start/,/^\s*#\s*Github Hosts End/d' "$hosts" 2>/dev/null
+    sed -i -E '/^[[:space:]]*#[[:space:]]*[gG]ithub[[:space:]]+[hH]osts[[:space:]]+[sS]tart[[:space:]]*$/,/^[[:space:]]*#[[:space:]]*[gG]ithub[[:space:]]+[hH]osts[[:space:]]+[eE]nd[[:space:]]*$/d' "$hosts"
     # 移除多余的空行
-    awk 'NF {n=0; print} !NF {if (!n) print; n=1}' "$hosts" > "$hosts.tmp" && mv "$hosts.tmp" "$hosts"
+    #awk 'NF {n=0; print} !NF {if (!n) print; n=1}' "$hosts" > "$hosts.tmp" && mv "$hosts.tmp" "$hosts"
 }
 
 add_github_hosts() {
@@ -34,28 +34,30 @@ add_github_hosts() {
     } >> "$hosts"
 
     # 移除多余的空行
-    awk 'NF {n=0; print} !NF {if (!n) print; n=1}' "$hosts" > "$hosts.tmp" && mv "$hosts.tmp" "$hosts"
+    tmp=$(mktemp)
+    awk 'NF {n=0; print} !NF {if (!n) print; n=1}' "$hosts" > "$tmp" && mv "$tmp" "$hosts"
+    [ -f "$tmp" ] && rm -f "$tmp"
 }
 
 get_github_dns() {
     uri_a=https://raw.githubusercontent.com/521xueweihan/GitHub520/refs/heads/main/hosts
     uri_b=https://githubhosts.xuanyuan.me
     # 从主要源获取
-    dns=$(curl -s "$uri_a" | grep -oiE '(([0-9]+\.){3}[0-9]+|[0-9a-f:]{8,}:[0-9a-f]{1,4})\s+(\w+\.)+\w+')
+    dns=$(curl -s "$uri_a" | grep -ioE '(([0-9]+\.){3}[0-9]+|[0-9a-f:]{8,}:[0-9a-f]{1,4})\s+(\w+\.)+\w+')
 
    # 如果主要源失败，从备用源获取
     if [ -z "$dns" ]; then
-        dns=$(wget -q -O - "$uri_b" | grep -oE '#Github Hosts Start.*?#Github Hosts End' | grep -ioE '(([0-9]+\.){3}[0-9]+|[0-9a-f:]{8,}:[0-9a-f]{1,4})\s+(\w+\.)+\w+')
+        dns=$(wget -q -O - "$uri_b" | grep -ioE '#Github\s+Hosts\s+Start.*?#Github\s+Hosts\s+End' | grep -ioE '(([0-9]+\.){3}[0-9]+|[0-9a-f:]{8,}:[0-9a-f]{1,4})\s+(\w+\.)+\w+')
     fi
     # 提取包含IPv4和IPv6的Github Hosts记录
-dns=$(wget -q -O - "$uri_b" | grep -oE '#Github Hosts Start.*?#Github Hosts End' | grep -oE '(([0-9]+\.){3}[0-9]+|(([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))\s+(\w+\.)+\w+')
+    dns=$(wget -q -O - "$uri_b" | grep -ioE '#Github\s+Hosts\s+Start.*?#Github\s+Hosts\s+End' | grep -ioE '(([0-9]+\.){3}[0-9]+|[0-9a-f:]{8,}:[0-9a-f]{1,4})\s+(\w+\.)+\w+')
 
     echo "$dns"
 }
 
 check_github_hosts() {
     hosts=$1
-    if grep -q -E "^\s*#\s*Github Hosts Start" "$hosts"; then
+    if grep -q -iE '^\s*#\s*Github\s+Hosts\s+Start\s*$' "$hosts"; then
         return 0  # 存在
     else
         return 1  # 不存在
@@ -81,6 +83,12 @@ main() {
         echo "Error: Failed to get GitHub DNS entries"
         exit 1
     fi
+
+    dns_local=$(sed -nE '/^[[:space:]]*#[[:space:]]*[gG]ithub[[:space:]]+[hH]osts[[:space:]]+[sS]tart[[:space:]]*$/,/^[[:space:]]*#[[:space:]]*[gG]ithub[[:space:]]+[hH]osts[[:space:]]+[eE]nd[[:space:]]*$/p' "$hosts" | grep -vE '^\s*#')
+    [ "$dns_local" = "$dns_github" ] && {
+        echo "GitHub DNS entries are already up to date. No changes needed."
+        exit 0
+    }
 
     if check_github_hosts "$hosts"; then
         echo "GitHub entries found in hosts file."
